79e171cb34fb9933d098ef5ea799c2afbfcc797c
Mikkobelly <mikiakuta0529@gmail.com>
2023-06-08 11:49:35 +0100

    refactor: move handleChange fn to ProductDetails; add title props for Main;

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,25 @@
 import ProductCard from "./ProductCard";
 import Basket from "./Basket";
 
-function Main({ products }) {
+function Main({ products, title }) {
     return (
-        <div className="page__flex">
-            <div className="flex--left">
-                <div className="card__grid">
-                    {products.map((item, i) => (
-                        <ProductCard
-                            key={i}
-                            product={item}
-                        />))}
+        <>
+            <h1 className="page__head">{title}</h1>
+            <div className="page__flex">
+                <div className="flex--left">
+                    <div className="card__grid">
+                        {products.map((item, i) => (
+                            <ProductCard
+                                key={i}
+                                product={item}
+                            />))}
+                    </div>
+                </div>
+                <div className="flex--right">
+                    <Basket />
                 </div>
             </div>
-            <div className="flex--right">
-                <Basket />
-            </div>
-        </div>
+        </>
     )
 }
 
