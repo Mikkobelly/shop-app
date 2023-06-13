127d8e569dd69a9cd5967eb2793c1cdd32855cfc
Mikkobelly <mikiakuta0529@gmail.com>
2023-06-13 09:15:36 +0100

    refactor: move handleAdd and handleRemove fn

--- src/components/App.js
+++ src/components/App.js
@@ -128,74 +128,13 @@ function App() {
     }
   }
 
-  // Run when item added to the basket
-  // This fn is defined here as it's needed in more than one component and it's passed as context
-  const handleAdd = (item, variantId) => {
-    // Return if no valid item is passed
-    if (!item || !variantId || variantId === 'default') {
-      alert('Please choose one from the option.');
-      return;
-    }
-
-    setBasketItems((prev) => {
-      // Check if the selected item is already added to the basket
-      const foundItem = prev.find(el => el.varId === variantId);
-
-      if (foundItem) {
-        foundItem.quantity++;
-        foundItem.totalVal += foundItem.price;
-        return prev;
-      } else {
-        const { title, variants } = item.node;
-        let index;
-        variants.edges.forEach((el, i) => {
-          if (el.node.id === variantId) {
-            index = i;
-          }
-        })
-
-        const entry = {
-          varId: variants.edges[index].node.id,
-          title,
-          varTitle: variants.edges[index].node.title,
-          image: variants.edges[index].node.image.url,
-          price: Number(variants.edges[index].node.price.amount),
-          currencyCode: variants.edges[index].node.price.currencyCode,
-          quantity: 1,
-          totalVal: Number(variants.edges[index].node.price.amount)
-        }
-
-        return basketItems.length === 0 ? [entry] : [...prev, entry];
-      }
-    })
-
-    // Update total price in basket
-    setTotalPrice((prev) => {
-      return prev + item.price || prev + Number(item.node.variants.edges[0].node.price.amount);
-    });
-  }
-
-  // Run when item removed from the basket
-  const handleRemove = (item, variantId) => {
-    setBasketItems((prev) => {
-      const foundItem = prev.find(el => el.varId === variantId);
-      foundItem.quantity--;
-      foundItem.totalVal -= foundItem.price;
-      return foundItem.quantity < 1 ? prev.filter(el => el !== foundItem) : prev;
-    })
-
-    setTotalPrice((prev) => {
-      return prev - item.price;
-    });
-  }
-
 
   return (
     <BrowserRouter>
       <NavigationBar />
 
       <AppContext.Provider
-        value={{ basketItems, totalPrice, handleAdd, handleRemove, selectedItem, setSelectedItem }}
+        value={{ basketItems, setBasketItems, totalPrice, setTotalPrice, selectedItem, setSelectedItem }}
       >
         <div className="content">
           <Routes>
--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { AppContext } from "./App";
 
 const ProductDetails = ({ products }) => {
-    const { handleAdd, selectedItem, setSelectedItem } = useContext(AppContext);
+    const { basketItems, setBasketItems, setTotalPrice, selectedItem, setSelectedItem } = useContext(AppContext);
 
     // Find which item to show its details
     const productTitle = decodeURIComponent(useParams().productTitle)
@@ -18,6 +18,59 @@ const ProductDetails = ({ products }) => {
             return item.node.id === e.target.value
         });
         setSelectedItem(foundVariant);
+    }
+
+    const handleAdd = (item, variantId) => {
+        // Return if no valid item is passed
+        if (!item || !variantId || variantId === 'default') {
+            alert('Please choose one from the option.');
+            return;
+        }
+
+        setBasketItems((prev) => {
+            // Check if the selected item is already added to the basket
+            const foundItem = prev.find(el => el.varId === variantId);
+
+            if (foundItem) {
+                foundItem.quantity++;
+                foundItem.totalVal += foundItem.price;
+                return prev;
+            } else {
+                const { title, variants } = item.node;
+                let index;
+                variants.edges.forEach((el, i) => {
+                    if (el.node.id === variantId) {
+                        index = i;
+                    }
+                })
+
+                const entry = {
+                    varId: variants.edges[index].node.id,
+                    title,
+                    varTitle: variants.edges[index].node.title,
+                    image: variants.edges[index].node.image.url,
+                    price: Number(variants.edges[index].node.price.amount),
+                    currencyCode: variants.edges[index].node.price.currencyCode,
+                    quantity: 1,
+                    totalVal: Number(variants.edges[index].node.price.amount)
+                }
+
+                return basketItems.length === 0 ? [entry] : [...prev, entry];
+            }
+        })
+
+        // Update total price in basket
+        setTotalPrice((prev) => {
+            return prev + item.price || prev + Number(item.node.variants.edges[0].node.price.amount);
+        });
+
+        // Reset selected state after adding the item to basket 
+        setSelectedItem(null);
+
+        // Set the selected value back to the default option
+        for (let i = 0; i < selectOptions.length; i++) {
+            selectOptions[i].selected = selectOptions[i].defaultSelected;
+        }
     }
 
 
@@ -39,15 +92,7 @@ const ProductDetails = ({ products }) => {
                 </select>
                 <button
                     className="details__add"
-                    onClick={() => {
-                        handleAdd(product, selectedItem ? selectedItem.node.id : null);
-                        // Reset selected state after adding the item to basket 
-                        setSelectedItem(null);
-                        // Set the selected value back to the default option
-                        for (let i = 0; i < selectOptions.length; i++) {
-                            selectOptions[i].selected = selectOptions[i].defaultSelected;
-                        }
-                    }}
+                    onClick={() => { handleAdd(product, selectedItem ? selectedItem.node.id : null); }}
                 >Add to Cart
                 </button>
             </div>
